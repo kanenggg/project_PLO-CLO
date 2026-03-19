d16805993b31c20e3a8e26c1cfb297def43ac138
kanenggg <ganfyeiei197@gmail.com>
2026-03-19 14:01:03 +0700

    Remove stale PLOCLO_cms submodule reference

--- PLOCLO_cms/backend/src/controllers/PLOreportControllers.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-
-export const getPloReport = async (req: any, res: any) => {
-  const { courseId } = req.query;
-};
